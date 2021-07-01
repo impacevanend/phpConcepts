9ebe194618a0d25b0d2c659a4ba890016236eab2
Joselo <impacevanend@gmail.com>
2021-07-01 17:57:40 -0500

    Se agrega estructura de datos de control

---
+++ operadores.php
@@ -0,0 +1,26 @@
+/*
+*De asignacion:
+
++=
+-=
+*=
+/=
+%=
+
+*De comparación:
+
+> mayor que
+< menor que
+>= mayor o igual que
+<= menor o igual que
+== Es igual asignacion
+!= Es diferente de
+
+
+*Operadores lógicos:
+
+&& -> And
+|| -> Or
+! -> Not
+xor -> Es verdaredo si las dos variables son diferentes
+*/
