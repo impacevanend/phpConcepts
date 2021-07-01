f2e5a98f96cd42c5c53b0061f5e969bf17c0a04a
Joselo <impacevanend@gmail.com>
2021-07-01 18:29:16 -0500

    Creación de CRUD

---
+++ php_crud_mysql/db.php
@@ -0,0 +1,8 @@
+/*
+*Verificar conexión
+Crear un proyeco nuevo en phpmyadmin
+Se crean cuatro campos (id, task, description, created_at(currentstanptime))
+
+*/
+#*Conexión a mysql(importar libreria)
+mysqli_connect('')
