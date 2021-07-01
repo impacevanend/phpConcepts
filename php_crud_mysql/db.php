83b78d45aaa96fbb47faeba1b1aa328f69261e23
Joselo <impacevanend@gmail.com>
2021-07-01 18:47:53 -0500

    Se añade conexión a BD, se adjunta framewok

--- php_crud_mysql/db.php
+++ php_crud_mysql/db.php
@@ -1,8 +1,32 @@
-/*
+<!-- /*
 *Verificar conexión
 Crear un proyeco nuevo en phpmyadmin
 Se crean cuatro campos (id, task, description, created_at(currentstanptime))
 
 */
-#*Conexión a mysql(importar libreria)
-mysqli_connect('')
+
+
+/*
+*Conexión a mysql(importar libreria)
+de manera local -> localhost
+usuario -> root
+pasajero -> ...
+base de datos -> php_mysql_cr♪d
+*/ -->
+
+<?php
+
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_mysql_crud'
+);
+
+// *Verificación de conexión
+// if (isset($conn)){
+//     echo "DB is connected";
+// }
+
+?>
