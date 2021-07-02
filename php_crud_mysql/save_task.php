ea28dc85937a006ef1f2bf17244188be6b7e9e0c
Joselo <impacevanend@gmail.com>
2021-07-02 12:35:25 -0500

    Se agrega consulta bd

--- php_crud_mysql/save_task.php
+++ php_crud_mysql/save_task.php
@@ -1,7 +1,23 @@
 <?php
 
+include('db.php');
+
+//* Comprobación de recibir datos
 if(isset($_POST['save_task'])){
-    echo 'saving';
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    
+    
+    $query = "INSERT INTO task(title,description) VALUES ('$title', '$description');
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        die('Query Failed');
+    }
+
+    echo 'saved';
 }
 
 ?>
+
