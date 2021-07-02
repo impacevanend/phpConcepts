6376a8d3eb12babb17820d4d63de1c8053fd482b
Joselo <impacevanend@gmail.com>
2021-07-02 12:00:07 -0500

    se crea comprobación con método POST

--- php_crud_mysql/index.php
+++ php_crud_mysql/index.php
@@ -13,15 +13,20 @@
           <div class="col-md-4">
           
               <div class="card card-body">
-
-                  <form action="#">
+                    <!--* Creación del formulario, datos enviados por medio de POST-> save_task.php -->
+                  <form action="save_task.php" method="POST">
 
                       <div class="form-group">
 
                           <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
-                      
                       </div>
-                  
+
+                      <div class="form-group">
+                          <textarea name="description" rows="2" class="form-control" placeholder="Task description"></textarea>
+                      </div>
+                      <div class="d-grid gap-2">
+                      <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
+                      </div>
                   </form>
               
               </div>
